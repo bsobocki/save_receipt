5e20600584ed8f6553e8bbd3540137b59792637c
bsobocki <bartq98@gmail.com>
2024-11-23 09:19:04 +0100

    add dummy buttons in top bar

--- lib/screen/receipt_data/components/top_bar.dart
+++ lib/screen/receipt_data/components/top_bar.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:save_receipt/screen/receipt_data/components/expandable_button.dart';
 
 class ReceiptPageTopBar extends StatelessWidget {
   const ReceiptPageTopBar(
@@ -41,11 +42,13 @@ class ReceiptPageTopBar extends StatelessWidget {
   }
 
   get popupMenu => PopupMenuButton<String>(
-    color: Colors.black,
-        onSelected: (String value){
+        color: Colors.black,
+        onSelected: (String value) {
           print("chosen: $value");
         },
-        itemBuilder: (context) => ['save receipt', 'edit item', 'remove item'].map((text) => getPopupMenuItem(text)).toList(),
+        itemBuilder: (context) => ['save receipt', 'edit item', 'remove item']
+            .map((text) => getPopupMenuItem(text))
+            .toList(),
         child: const Icon(Icons.menu),
       );
 
@@ -72,12 +75,38 @@ class ReceiptPageTopBar extends StatelessWidget {
         const SizedBox(height: 20),
         SizedBox(
           height: 160,
-          child: IconButton(
-            onPressed: onImageIconPress,
-            icon: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: receiptIcon,
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: onImageIconPress,
+                icon: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: receiptIcon,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ExpandableButton(
+                        buttonColor: Colors.red,
+                        iconData: Icons.qr_code,
+                        onPressed: () {},
+                        label: 'Add barcode'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ExpandableButton(
+                        buttonColor: Colors.cyan,
+                        iconData: Icons.add,
+                        onPressed: () {},
+                        label: 'Add Item'),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ],
